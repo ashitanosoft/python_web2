fa1e264f65659e446c2b676c38be5196e677f116
ashitanosoft <measuresoft20151203@gmail.com>
2020-05-29 11:28:03 +0900

    add @app.route

--- app/app.py
+++ app/app.py
@@ -1,4 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+
+#Flaskオブジェクトの生成
+app = Flask(__name__)
+
+#「/」へアクセスがあった場合に、"Hello world" の文字列を返す
+@app.route("/")
+def hello():
+    return "Hello World"
+
+
+@app.route("/index")
+def index():
+    return render_template("index.html")
 
 
 
